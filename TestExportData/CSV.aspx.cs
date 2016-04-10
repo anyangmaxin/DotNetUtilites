31395e82a8d520cda8b6a2e5b95606a0bc8c4163
anyangmaxin@163.com <anyangmaxin@163.com>
2016-04-10 16:45:12 +0800

    修改导出csv类

---
+++ TestExportData/CSV.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ExportDataToXXX;
+
+namespace TestExportData
+{
+    public partial class CSV : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+ 
+    }
+}
